2a7204a952368c22db6af9e3b1c65aa07d67d66a
iyanuoluwa sowande <iyanusowande@gmail.com>
2021-12-05 06:25:50 +0100

    new components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <Navigation />
   <Layout>
-    <router-view />
+    <!-- <router-view /> -->
+
+    <HeroSection />
+    <About />
   </Layout>
-  <!-- <Layout/> -->
 </template>
 <script>
-  import "boxicons/css/boxicons.min.css";
-  import Navigation from "@/components/Navigation.vue";
+import 'boxicons/css/boxicons.min.css';
+import Layout from '@/components/Layout.vue';
+import About from '@/components/sections/About.vue';
+import Navigation from '@/components/Navigation.vue';
+import HeroSection from '@/components/sections/HeroSection.vue';
 
-  export default {
-    name: "App",
-    components: {
-      Navigation,
-    },
-    setup() {},
-  };
+export default {
+  name: 'App',
+  components: {
+    Navigation,
+    Layout,
+    HeroSection,
+    About,
+  },
+  setup() {},
+};
 </script>
 <style></style>
