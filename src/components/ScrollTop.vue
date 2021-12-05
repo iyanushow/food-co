6a0a79d258678516b92e700a9262aa992ac49019
iyanuoluwa sowande <iyanusowande@gmail.com>
2021-12-05 06:25:14 +0100

    scroll top and about components

---
+++ src/components/ScrollTop.vue
@@ -0,0 +1,60 @@
+<template>
+  <a href="" class="scrollTop" :class="{ 'scroll-top': changeHeader }" id="scroll-top">
+    <box-icon name="chevron-up" class="scrollIcon"></box-icon>
+  </a>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+export default {
+  setup() {
+    const changeHeader = ref(false);
+    const windowOffset = ref(window.scrollY);
+
+    watch(windowOffset, (newVal, oldVal) => {
+      console.log(oldVal, newVal);
+      if (newVal >= 560) {
+        // add scrollheader class
+        changeHeader.value = true;
+      } else {
+        changeHeader.value = false;
+      }
+    });
+
+    return {
+      changeHeader,
+    };
+  },
+};
+</script>
+
+<style>
+.scrollTop {
+  position: fixed;
+  right: 1rem;
+  bottom: -20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.3rem;
+  background: #069c5480;
+  border-radius: 4px;
+  z-index: var(--z-tooltip);
+  transition: 0.4s;
+  visibility: hidden;
+}
+
+.scrollTop:hover {
+  background: var(--main-alt);
+}
+
+.scrollIcon {
+  font-size: 1.2rem;
+  fill: var(--body-color);
+}
+
+.scroll-top {
+  visibility: visible;
+  bottom: 1rem;
+}
+</style>
---
+++ src/components/sections/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about section container" id="about">
+    <div class="about__container bd-grid">
+      <div class="about__data">
+        <span class="section-subtitle">About us</span>
+        <h2 class="section-title">
+          We make the best <br />
+          tasty food
+        </h2>
+        <p class="about_-description">Legendary cuisine to satisfy your cravings</p>
+        <a href="#" class="button">Explore History</a>
+      </div>
+
+      <img src="assets/about.jpg" alt="" class="about__img" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.about__data {
+  text-align: center;
+}
+.about__description {
+  margin-bottom: var(--m-3);
+}
+.about__img {
+  width: 280px;
+  border-radius: 0.5rem;
+}
+</style>
